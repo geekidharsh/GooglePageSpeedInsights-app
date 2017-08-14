1a26bb10841a594fc5715efd35d40cb1f3dab3e8
Harsh Pandey <harshvardhanpandey@hotmail.com>
2017-08-14 14:48:03 -0400

    Update page-speed-app-test-properties_v1.py

--- page-speed-app-test-properties_v1.py
+++ page-speed-app-test-properties_v1.py
@@ -15,7 +15,7 @@ import numpy as np
 # pagespeed api to call
 API_URL = 'https://www.googleapis.com/pagespeedonline/v2/runPagespeed?' 
 # user credentials
-api_key = 'AIzaSyDiBn4WVS3zZ2E4LtKElbaf17E577qVwrI'
+api_key = 'yourapikeygoeshere'
 
 # call properties aka list of urls from a csv file here
 properties = pd.read_csv('filename.csv')
